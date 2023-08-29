f1e8e887b16090797f93da4d1908e8fe9469909c
nxhuel <nahueltisera03@gmail.com>
2023-08-29 13:09:57 -0300

    Agregando ejercicio de punteros

---
+++ seccion-1/punteros/punteros.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main () {
+	a, b := 10, 20
+
+	fmt.Println("Valores iniciales: a =", a, ", b =", b)
+
+	ptrA := &a
+    *ptrA, b = b, *ptrA
+	
+	fmt.Println("Valores intercambiados: a = ", a, ", b =", b)
+}
