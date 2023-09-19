38bdbf15a8fa6928c3eab91d7fa493510db55aeb
nxhuel <nahueltisera03@gmail.com>
2023-09-19 15:54:15 -0300

    Agregando v2 al ejercicio 'contador de palabras'

--- seccion-2/word-count.go
+++ seccion-2/word-count.go
@@ -1,22 +1,61 @@
+// V.1
+
+// package main
+
+// import(
+// 	"fmt"
+// 	"strings"
+// )
+
+// func contador(){
+// 	var s string
+// 	s = "I ate a donut. Then I ate another donut."
+// 	palabras := strings.Fields(s)
+
+// 	resultado := make(map[string]int)
+
+// 	for _, palabra := range palabras {
+// 		resultado[palabra]++
+// 	}
+// 	fmt.Println(resultado)
+// }
+
+// func main(){
+// 	contador()
+// }
+
+// V.2
+
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
-func main() {
-	s := "I ate a donut. Then I ate another donut."
-	resultado := contarPalabras(s)
-	fmt.Println(resultado)
+func solicitud() string{
+	var oracion string
+	fmt.Println("Ingrese una oracion:")
+	lectura := bufio.NewReader(os.Stdin)
+	oracion, _ = lectura.ReadString('\n')
+	return oracion
 }
 
-func contarPalabras(s string) map[string]int {
-	palabras := strings.Fields(s)
+func contador(){
+	var x string
+	x = solicitud()
+	palabras := strings.Fields(x)
+
 	resultado := make(map[string]int)
 
 	for _, palabra := range palabras {
 		resultado[palabra]++
 	}
-	return resultado
+	fmt.Println(resultado)
+}
+
+func main(){
+	contador()
 }
